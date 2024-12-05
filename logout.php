1620a6af9de8bc191ab175129e7bd085283b5119
張文謙 <chesteras452@gmail.com>
2024-12-05 10:00:53 +0800

    登出

---
+++ logout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="2; url=main.php">
+    <title>通知</title>
+</head>
+
+<body background="home-bg.jpg">
+    <?php
+    $_SESSION['ID'] = "";
+    $_SESSION['password'] = "";
+    $_SESSION['name'] = "";
+    $_SESSION['level'] = "";
+    $_SESSION['password'] = "";
+
+    echo "<h1 align='center'>登出成功</h1>";
+    ?>
+
+</body>
+
+</html>
